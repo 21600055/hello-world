a9d49c56e5b72782a4deb39faa5413029d57c8dd
21600055 <21600055@handong.edu>
2021-09-05 11:54:56 +0900

    second commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -6,10 +6,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.print("ÀÌ¸§ ÀÔ·Â : ");
+		System.out.print("ì´ë¦„ ì…ë ¥ : ");
 		Scanner s = new Scanner(System.in);
 		String name = s.next();
-		System.out.print("["+name+"]´Ô È¯¿µÇÕ´Ï´Ù~");
+		System.out.print("["+name+"]");
 		s.close();
 	}
 }
